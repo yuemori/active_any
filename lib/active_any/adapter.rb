6062cc0a6722b2f9fe6705a45d49eb4591ead3a8
yuemori <yuemori@aiming-inc.com>
2017-07-20 02:53:04 +0900

    Fix Adapter no wrapping for ActiveAny

--- lib/active_any/adapter.rb
+++ lib/active_any/adapter.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-class Adapter
-  def initialize(klass)
-    @klass = klass
-  end
-
-  def query(where_clause, limit_value)
-    records = @klass.load
-
-    records = records.select do |record|
-      where_clause.all? do |condition|
-        condition.match?(record)
-      end
+module ActiveAny
+  class Adapter
+    def initialize(klass)
+      @klass = klass
     end
 
-    limit_value ? records.take(limit_value) : records
+    def query(where_clause, limit_value)
+      records = @klass.load
+
+      records = records.select do |record|
+        where_clause.all? do |condition|
+          condition.match?(record)
+        end
+      end
+
+      limit_value ? records.take(limit_value) : records
+    end
   end
 end
