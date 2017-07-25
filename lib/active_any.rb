5edecaeb0f94a3497f579d6b1982027f54105962
yuemori <yuemori@aiming-inc.com>
2017-07-25 10:27:31 +0900

    Split base class

--- lib/active_any.rb
+++ lib/active_any.rb
@@ -12,6 +12,7 @@ require 'active_any/association_relation'
 require 'active_any/attribute_assignment'
 require 'active_any/finders'
 require 'active_any/core'
+require 'active_any/base'
 require 'active_any/subscriber'
 require 'active_any/adapter'
 require 'active_any/adapters/abstract_adapter'
@@ -24,49 +25,5 @@ module ActiveAny
 
   def self.config
     @config ||= Configuration.new
-  end
-
-  class Base
-    include Core
-    include Associations
-    include AttributeAssignment
-    include Finders
-    include Reflection
-  end
-
-  class Object < Base
-    def self.inherited(child)
-      child.abstract_class = false
-    end
-
-    class << self
-      attr_accessor :data
-
-      def adapter
-        @adapter ||= ObjectAdapter.new(self)
-      end
-    end
-  end
-
-  class Hash < Base
-    def self.inherited(child)
-      child.abstract_class = false
-    end
-
-    class << self
-      attr_reader :data
-
-      def data=(data)
-        data.map(&:keys).flatten.each do |method|
-          attr_accessor method
-        end
-
-        @data = data.map { |d| new(d) }
-      end
-
-      def adapter
-        @adapter ||= ObjectAdapter.new(self)
-      end
-    end
   end
 end
---
+++ lib/active_any/base.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module ActiveAny
+  class Base
+    include Core
+    include Associations
+    include AttributeAssignment
+    include Finders
+    include Reflection
+  end
+
+  class Object < Base
+    def self.inherited(child)
+      child.abstract_class = false
+    end
+
+    class << self
+      attr_accessor :data
+
+      def adapter
+        @adapter ||= ObjectAdapter.new(self)
+      end
+    end
+  end
+
+  class Hash < Base
+    def self.inherited(child)
+      child.abstract_class = false
+    end
+
+    class << self
+      attr_reader :data
+
+      def data=(data)
+        data.map(&:keys).flatten.each do |method|
+          attr_accessor method
+        end
+
+        @data = data.map { |d| new(d) }
+      end
+
+      def adapter
+        @adapter ||= ObjectAdapter.new(self)
+      end
+    end
+  end
+end
