175b5518f67812de3726b35b5479b3411459d160
yuemori <yuemori@aiming-inc.com>
2017-07-25 14:19:06 +0900

    Implement ActiveAny::CSV

--- lib/active_any/base.rb
+++ lib/active_any/base.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'csv'
+require 'pathname'
+
 module ActiveAny
   class Base
     include Core
@@ -38,6 +41,35 @@ module ActiveAny
         end
 
         @data = data.map { |d| new(d) }
+      end
+
+      def adapter
+        @adapter ||= ObjectAdapter.new(self)
+      end
+    end
+  end
+
+  class CSV < Base
+    def self.inherited(child)
+      child.abstract_class = false
+    end
+
+    class MissingFileError; end
+
+    class << self
+      attr_accessor :file
+
+      def data
+        @data ||= begin
+          raise MissingFileError unless file
+
+          table = ::CSV.table(file)
+          table.headers.each do |header|
+            attribute header
+          end
+
+          table.map { |row| new(row.to_h) }
+        end
       end
 
       def adapter
