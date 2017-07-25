a9a0fd8bed1c8b5d72a716b67c56165080fca694
yuemori <yuemori@aiming-inc.com>
2017-07-25 11:27:18 +0900

    Remove unecessary abstract handler

--- lib/active_any/adapters/abstract_adapter.rb
+++ lib/active_any/adapters/abstract_adapter.rb
@@ -11,17 +11,5 @@ module ActiveAny
     def query(_where_clause, _limit_value = nil, _group_values = [], _order_values = [])
       raise NotImplementedError.new, "#{self.class.name} can not handle for #{value.class}"
     end
-
-    def limit_handler(_records, _limit_value)
-      raise NotImplementedError.new, "#{self.class.name} can not handle for limit"
-    end
-
-    def group_handler(_records, _group_values)
-      raise NotImplementedError.new, "#{self.class.name} can not handle for group"
-    end
-
-    def order_handler(_records, _order_values)
-      raise NotImplementedError.new, "#{self.class.name} can not handle for order"
-    end
   end
 end
